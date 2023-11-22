26d6d98a6605f5df6d5b6ee374cc7b49c955287a
raiane2006 <146495765+raiane2006@users.noreply.github.com>
2023-11-22 14:19:32 -0300

    projeto estaçoes

--- main.js
+++ main.js
@@ -2,3 +2,24 @@ const aplausos = document.querySelector("#som_tecla_aplausos");
 function tocaSom(aplausos){
     alert(aplausos);
 }
+function digaSeuNome(Raiane){
+    alert(Raiane);
+}
+digaSeuNome("Meu nome é Raiane");
+
+let comando ="tocar"
+if (comando =="tocar"){
+    alert("Áudio pronto para tocar");
+
+}
+else{
+    alert("O áudio não está pronto");
+
+}
+const buttons = document.querySelectorAll("button");
+
+let contador =0;
+while(contador<buttons.length){
+    buttons[contador].textContent = contador;
+    contador = contador+1;
+}
